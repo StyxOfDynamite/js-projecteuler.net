ef0cc211d06705e4dd699b81f55bab5d225e7948
Luke Storer <StyxOfDynamite@users.noreply.github.com>
2017-02-17 14:20:40 +0000

    Update 2.js

--- 2.js
+++ 2.js
@@ -5,13 +5,13 @@ var max = 4000000;
 var i = 2;
 var loop = true
 
-while (fib[fib.length-1] < max && loop) {
+while (loop) {
 	if (fib[i-1] + fib[i-2] > max) {
   		loop = false;
   	} else {
 		fib.push(fib[i-1] + fib[i-2]);
   		i++;
-	}
+    }
 }
 
 console.log(fib);
