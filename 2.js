4047d67d4d45c336b788f284f8f130ed3bf0f1b4
Luke Storer <StyxOfDynamite@users.noreply.github.com>
2017-02-17 14:11:04 +0000

    Update 2.js
    Start sequence with 1 and 2 rather than 0 and 1

--- 2.js
+++ 2.js
@@ -1,16 +1,16 @@
 //
 
-var fib = [0,1];
+var fib = [1,2];
 var max = 4000000;
 var i = 2;
 var loop = true
 
 while (fib[fib.length-1] < max && loop) {
 	if (fib[i-1] + fib[i-2] > max) {
-  	loop = false;
-  } else {
+  		loop = false;
+  	} else {
 		fib.push(fib[i-1] + fib[i-2]);
-  	i++;
+  		i++;
 	}
 }
 
