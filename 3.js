73a9c609685fa02f64a688b4c8ffd59df7d670a3
lukes <lukestorer.reviews@gmail.com>
2017-02-22 11:15:29 +0000

    Reduce time complexity of finding factors.

--- 3.js
+++ 3.js
@@ -1,37 +1,40 @@
 // The prime factors of 13195 are 5, 7, 13 and 29.
 // What is the largest prime factor of the number 600851475143 ?
 
+// Takes a number, n, and returns a array of
+// all the factors of n.
 function factorsOf(n) {
-	var factors = [];
-	for (var i = 1; i <= n; i++) {
-		if (n % i === 0) {
-  		factors.push(i);
-  	}
-	}
-  return factors;
-}
-
-function isPrime(number) {
-	for (var i=2; i < number; i++) {
-  	if (number % i === 0) {
-    	return false;
+    var factors = [];
+    for (var i = 2; i <= Math.sqrt(n); i++) {
+        if (n % i === 0) {
+            factors.push(i);
+            factors.push(n/i);
+        }
     }
-  }
-  return number > 1;
+    return factors;
 }
 
-function primes(factors) {
-	var primes = [];
-	for (var i=1; i<factors.length; i++) {
-  	if (isPrime(factors[i])) {
-    	primes.push(factors[i]);
+// Takes a array of proper factors for some number, n,
+// and returns a array of the prime factors of n.
+function factorsToPrimes(factors) {
+    var primes = [];
+    // Go through each factor
+    for (var i=0; i<factors.length; i++) {
+        var iIsPrime = true;
+        // Check to see if any other factor divides it
+        for (var j=0; j<factors.length; j++) {
+            if (factors[i]%factors[j] == 0 && i != j)
+                iIsPrime = false;
+        }
+        // If no other factor divides it, it must be prime
+        if (iIsPrime)
+            primes.push(factors[i]);
     }
-  }
-  return primes;
+    return primes;
 }
 
+var primeFactors = factorsToPrimes(factorsOf(600851475143));
 
-var factors = factorsOf(600851475143);
-var arr = primes(factors);
+primeFactors.sort(function(a, b){return a-b});
 
-console.log(arr[arr.length-1]);
+console.log(primeFactors[primeFactors.length-1]);
