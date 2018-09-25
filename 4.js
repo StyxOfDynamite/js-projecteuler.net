99fd0e97e8f3db194fd96f9f52c20558b3250992
lukes <luke@reviews.co.uk>
2018-09-25 16:47:04 +0100

    updated 4.js.

--- 4.js
+++ 4.js
@@ -1,27 +1,31 @@
 // A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
 // Find the largest palindrome made from the product of two 3-digit numbers.
 
-<?php
-
-function isPalindrome($input) {
-    return (string) $input === strrev($input);
+// helper function to determine if a number is palindromic by casting it to a string
+//  and comparing it against itself reversed
+function isPalindrome(input) {
+    input = String(input);
+    return input === input.split('').reverse().join('');
 }
 
-$max = 999;
-$min = 100;
-$found = 0;
+// initial state
+var min = 100;
+var max = 999;
+var found = 0;
+var a = 0;
+var b = 0;
 
-for ($i = $max; $i >= $min; $i--) {
-    for ($j = $max; $j >= $min; $j--) {
-        $result = $i * $j;
-        if (isPalindrome($result)) {
-            if ($result > $found) {
-                $found = $result;
-                $a = $i;
-                $b = $j;
+for (var i = max; i >= min; i--) {
+    for (var j = max; j >= min; j--) {
+        var result = i * j;
+        if (isPalindrome(result)) {
+            if (result > found) {
+                found = result;
+                a = i;
+                b = j;
             }
         }
     }
 }
 
-print sprintf("Found %d times %d = %d", $a, $b, $found);
+console.log(a + ' times ' + b + ' = ' + found);
